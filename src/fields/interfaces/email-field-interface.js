0173a24d03ca6f56e51475668b1790cdce41e1c8
Tuan Le <tuan.t.lei@gmail.com>
2016-11-02 23:59:15 -0700

    Updated license

--- src/fields/interfaces/email-field-interface.js
+++ src/fields/interfaces/email-field-interface.js
@@ -1,7 +1,17 @@
 /**
- * Copyright (c) 2016-present, Boki, Inc. All rights reserved.
+ * Copyright 2016-present Tuan Le.
  *
- * Licensed TBA
+ * Licensed under the MIT License.
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/mit-license.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  *------------------------------------------------------------------------
  *
