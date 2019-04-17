1fd208489e9da6bf2c4ca85215a96beeee702fb1
Tuan Le <tuan.t.lei@gmail.com>
2019-04-17 13:00:33 -0700

    Adding tests

---
+++ __tests__/test-suites.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * Tests Suites.
+ *
+ * @description - Test suites runner for unit testings.
+ *
+ * @author Tuan Le (tuan.t.lei@gmail.com)
+ *
+ */
+'use strict'; // eslint-disable-line
+
+/* load babel */
+/* eslint quotes: 0 */
+require('babel-register');
+
+/* load and initialize hypertoxin */
+require('hypertoxin').init();
+
+/* load test specs for Ht modules */
